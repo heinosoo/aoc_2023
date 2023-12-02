e2b923d2c5d804b4ad64f15592667e8dae3f077f
Heino <34691383+heinosoo@users.noreply.github.com>
2023-12-02 15:39:39 +0000

    Day 1

---
+++ day_01/part1.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"strconv"
+)
+
+func Part1(input <-chan string, output chan string) {
+	sum := 0
+	for line := range input {
+		code := parse(line)
+		// log.Println(code)
+		sum += code
+	}
+
+	output <- strconv.Itoa(sum)
+	close(output)
+}
+
+func parse(line string) int {
+	code, _ := strconv.Atoi(firstDigit(line) + lastDigit(line))
+	return code
+}
+
+func firstDigit(line string) string {
+	for i := 0; i < len(line); i++ {
+		if line[i] >= '0' && line[i] <= '9' {
+			return string(line[i])
+		}
+	}
+	// panic("no first digit found for line: " + line)
+	return "0"
+}
+func lastDigit(line string) string {
+	for i := len(line) - 1; i >= 0; i-- {
+		if line[i] >= '0' && line[i] <= '9' {
+			return string(line[i])
+		}
+	}
+	// panic("no last digit found for line: " + line)
+	return "0"
+}
---
+++ day_01/part2.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"log"
+	"regexp"
+	"strconv"
+	"strings"
+)
+
+func Part2(input <-chan string, output chan string) {
+	sum := 0
+	for line := range input {
+		code := parse2(line)
+		// log.Println(line, code)
+		sum += code
+	}
+
+	log.Println(sum)
+	output <- strconv.Itoa(sum)
+	close(output)
+}
+
+func parse2(line string) int {
+	// Hack to deal with overlapping matches
+	line = strings.ReplaceAll(line, "one", "oonee")
+	line = strings.ReplaceAll(line, "eight", "eeightt")
+
+	allDigitsOrDigitNames := regexp.MustCompile(`(\d|one|two|three|four|five|six|seven|eight|nine)`).FindAllString(line, -1)
+
+	firstDigit := DIGIT_NAMES[allDigitsOrDigitNames[0]]
+	lastDigit := DIGIT_NAMES[allDigitsOrDigitNames[len(allDigitsOrDigitNames)-1]]
+	code, _ := strconv.Atoi(firstDigit + lastDigit)
+	return code
+}
+
+var DIGIT_NAMES = map[string]string{
+	"one":   "1",
+	"two":   "2",
+	"three": "3",
+	"four":  "4",
+	"five":  "5",
+	"six":   "6",
+	"seven": "7",
+	"eight": "8",
+	"nine":  "9",
+	"1":     "1",
+	"2":     "2",
+	"3":     "3",
+	"4":     "4",
+	"5":     "5",
+	"6":     "6",
+	"7":     "7",
+	"8":     "8",
+	"9":     "9",
+}
